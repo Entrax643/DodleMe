6175bc108b2d66d6e339c619afefe82964eca6cb
EmmaMurilloCantie <emma.murillocantie@gmail.com>
2017-02-28 14:08:50 +0100

    Ajout fichier front

---
+++ Front/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('dodleme',['ui.router']);
+app.config(function($stateProvider) {
+  var helloState = {
+    name: 'liste',
+    url: '/liste',
+    templateUrl: './vues/listeevent.html'
+  }
+
+  var aboutState = {
+    name: 'creer',
+    url: '/creer',
+    templateUrl: './vues/creerevent.html'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
